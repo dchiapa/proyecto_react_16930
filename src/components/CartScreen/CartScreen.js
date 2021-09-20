896ae6f50e4f134acffb8bc05e7920fb504be650
dchiapa <dchiapa@est.frba.utn.edu.ar>
2021-09-20 20:32:20 -0300

    actualizar renderizado de la vista del carrito

--- src/components/CartScreen/CartScreen.js
+++ src/components/CartScreen/CartScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
+import "./css/CartScreen.css";
 
 export const CartScreen = () => {
   const { cart, removeFromCart, clearCart } = useContext(CartContext);
@@ -12,23 +13,64 @@ export const CartScreen = () => {
   }
 
   return (
-    <main className="main">
-      <h1>Resumen de compra</h1>
-
+    <main className="main main__cart">
+      <h1 className="main__title">Resumen de compra</h1>
       {cart.length > 0 ? (
         <>
-          {cart.map((prod) => (
-            <article key={prod.id}>
-              <h3>{prod.name}</h3>
-              <p>Cantidad: {prod.count}</p>
-              <p>Precio: ${prod.price * prod.count}</p>
-              <button onClick={() => removeFromCart(prod.id)}>Eliminar</button>
-            </article>
-          ))}
-          <button className="" onClick={clearCart}>
-            Vaciar carrito
-          </button>
-          <Link to="/checkout">Terminar compra</Link>
+          <table>
+            <thead>
+              <tr>
+                <th>Producto</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+                <th>Total</th>
+                <th>Eliminar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((prod) => (
+                <tr key={prod.id}>
+                  <td>{prod.name}</td>
+                  <td>${prod.price}</td>
+                  <td>{prod.count}</td>
+                  <td>${prod.count * prod.price}</td>
+                  <td>
+                    <button
+                      className="btn"
+                      onClick={() => removeFromCart(prod.id)}
+                    >
+                      Eliminar
+                    </button>
+                  </td>
+                </tr>
+              ))}
+              <tr>
+                <td colSpan="5" className="row__space"></td>
+              </tr>
+              <tr>
+                <td colSpan="3">Total carrito</td>
+                <td>
+                  $
+                  {cart.reduce(
+                    (total, prod) => total + prod.price * prod.count,
+                    0
+                  )}
+                </td>
+                <td>
+                  <button className="btn cart__btn" onClick={clearCart}>
+                    Vaciar carrito
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="5">
+                  <Link className="btn cart__btn" to="/checkout">
+                    Terminar compra
+                  </Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </>
       ) : (
         <p>No hay productos en el carrito</p>
